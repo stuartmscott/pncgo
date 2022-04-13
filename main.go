59e3f73cfa3328207507e0876990cea384e2eda6
Stuart Scott <stuart.murray.scott@gmail.com>
2022-04-13 09:59:08 -0700

    Step 3: Win Streak

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
 	// Parse operation from args
+	var operation func(int, int) int
+	switch os.Args[1] {
+	case "+":
+		operation = func(a, b int) int {
+			return a + b
+		}
+	case "-":
+		operation = func(a, b int) int {
+			return a - b
+		}
+	case "*":
+		operation = func(a, b int) int {
+			return a * b
+		}
+	case "/":
+		operation = func(a, b int) int {
+			return a / b
+		}
+	case "%":
+		operation = func(a, b int) int {
+			return a % b
+		}
+	}
 	// Parse parameters from args
+	a, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Calculate result
+	result := operation(a, b)
 	// Print result
-	log.Println("Success!")
+	log.Println(result)
 }
